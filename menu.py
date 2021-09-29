672d31f985bbdb439f36a0363bc9cd29b1f28e36
dosevaiva <90329505+dosevaiva@users.noreply.github.com>
2021-09-29 17:11:50 +0200

    Add validation for GamePredictor

--- menu.py
+++ menu.py
@@ -52,9 +52,24 @@ class Menu():
                 self.user_team.data_visual.linechart(a, elements)
                 continue
             elif user_option_main_menu == '7':
+                while True:
+                    game=input('Where does the first team play? \n 1. Home \n 2. Road \n' )
+                    if game == '1' or game == '2':
+                        break
+                    else:
+                        print('This is not a valid option! Try again!')
+                        continue
 
-                game=input('Where does the first team play? \n 1. Home \n 2. Road \n' )
-                opp_team = input("Enter the opposing team: ").title()
+                while True:
+                    opp_team = input("Enter the opposing team: ").title()
+                    self.user_team = Team(opp_team)
+                    #print(self.team_row)
+                    if len(self.user_team.team_row) == 0:
+                        print('This is not a valid team name!')
+                        continue
+                    else:
+                        break
+    
                 predict= GamePredictor(self.user_team, game, opp_team)
                 predict.predict()
                 continue
